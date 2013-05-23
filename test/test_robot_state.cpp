8648aac4362e6354de21f08b71b3ef35ce4a13e2
Eric Huang <ehuang4@gmail.com>
2013-05-23 12:11:25 -0400

    test: utils for testing

--- test/test_robot_state.cpp
+++ test/test_robot_state.cpp
@@ -1,18 +1,4 @@
-#include <iostream>
-#include <gtest/gtest.h>
-
-#include <robotics/parser/dart_parser/DartLoader.h>
-#include <simulation/World.h>
-#include <kinematics/Skeleton.h>
-#include <kinematics/Dof.h>
-#include <kinematics/BodyNode.h>
-#include <kinematics/Joint.h>
-#include <kinematics/Transformation.h>
-#include <dynamics/SkeletonDynamics.h>
-
-#include <utils/data_paths.h>
-#include <robot/robot_state.h>
-#include <atlas/atlas_state.h>
+#include "test_utils.h"
 
 using namespace std;
 using namespace Eigen;
@@ -23,18 +9,6 @@ using namespace dynamics;
 
 using namespace robot;
 using namespace atlas;
-/* ********************************************************************************************* */
-#define ROBOT_URDF "models/atlas/atlas_world.urdf"
-#define ROBOT_NAME "atlas"
-kinematics::Skeleton* _robot_;
-Skeleton* PREPARE_ROBOT() {
-    if(_robot_ == 0) {
-        DartLoader dart_loader;
-		World *mWorld = dart_loader.parseWorld(VRC_DATA_PATH ROBOT_URDF);
-		_robot_ = mWorld->getSkeleton(ROBOT_NAME);
-    }
-    return _robot_;
-}
 /* ********************************************************************************************* */
 TEST(STATE, TEST_INIT) {
     atlas_state_t as;
@@ -50,39 +24,19 @@ TEST(STATE, TEST_D_BODY) {
     Isometry3d Twb;
     Twb = Matrix4d::Identity();
 
-    for(int i=3; i < 6; i++) {
-        // cout << robot->getDof(i)->getName() << endl;
-    }
-    
     VectorXd dofs = robot->getPose();
     dofs.setZero();
-
     dofs(3) = 1;
     dofs(4) = M_PI/2;
     dofs(5) = 2;
-
     robot->setPose(dofs);
 
     Twb = robot->getNode("pelvis")->getWorldTransform();
 
-    BodyNode *pelvis = robot->getNode("pelvis");
-    Joint* joint = pelvis->getParentJoint();
-    for(int i=0; i < joint->getNumTransforms(); i++) {
-        Transformation *xform = joint->getTransform(i);
-        cout << xform->getName() << " = \n" << xform->getTransform() << endl;
-    }
-
-    cout << endl;
-
-    cout << "Twb = \n" << Twb.matrix() << endl;
-
     as.set_d_body(Twb);
-
-    cout << "pose = \n" << as.d_pose().block<6,1>(0,0) << endl;
-
     robot->setPose(as.d_pose());
-    
-    cout << "Twb = \n" << robot->getNode("pelvis")->getWorldTransform() << endl;
+
+    ASSERT_MATRIX_EQ(Twb.matrix(), robot->getNode("pelvis")->getWorldTransform());
 }
 /* ********************************************************************************************* */
 int main(int argc, char* argv[]) {
---
+++ test/test_utils.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <iostream>
+#include <stdio.h>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+#include <gtest/gtest.h>
+
+#include <atlas/atlas_kinematics.h>
+#include <utils/math_utils.h>
+#include <utils/data_paths.h>
+#include <math/EigenHelper.h>
+
+#include <math.h>
+
+#include <robotics/parser/dart_parser/DartLoader.h>
+#include <simulation/World.h>
+#include <kinematics/Skeleton.h>
+#include <kinematics/Dof.h>
+#include <kinematics/BodyNode.h>
+#include <kinematics/Joint.h>
+#include <kinematics/Transformation.h>
+#include <dynamics/SkeletonDynamics.h>
+
+#include <atlas/atlas_jacobian.h>
+#include <robot/robot_state.h>
+#include <atlas/atlas_state.h>
+
+/* ********************************************************************************************* */
+#define ROBOT_URDF "models/atlas/atlas_world.urdf"
+#define ROBOT_NAME "atlas"
+#define LEFT_HAND "l_hand"
+#define RIGHT_HAND "r_hand"
+#define ROBOT_JACOBIAN_T atlas::atlas_jacobian_t
+#define ROBOT_STATE_T atlas::atlas_state_t
+#define ROBOT_KINEMATICS_T atlas::atlas_kinematics_t
+/* ********************************************************************************************* */
+kinematics::Skeleton* _robot_;
+kinematics::Skeleton* PREPARE_ROBOT() {
+    if(_robot_ == 0) {
+        DartLoader dart_loader;
+        simulation::World *mWorld = dart_loader.parseWorld(VRC_DATA_PATH ROBOT_URDF);
+		_robot_ = mWorld->getSkeleton(ROBOT_NAME);
+    }
+    return _robot_;
+}
+/* ********************************************************************************************* */
+ROBOT_JACOBIAN_T* _rj_;
+robot::robot_jacobian_t* PREPARE_ROBOT_JACOBIAN() {
+    if(_rj_ == 0) {
+        _rj_ = new ROBOT_JACOBIAN_T;
+        _rj_->init(PREPARE_ROBOT());
+    }
+    return _rj_;
+}
+/* ********************************************************************************************* */
+ROBOT_STATE_T* _rs_;
+robot::robot_state_t* PREPARE_ROBOT_STATE() {
+    if(_rs_ == 0) {
+        _rs_ = new ROBOT_STATE_T;
+        _rs_->init(PREPARE_ROBOT());
+    }
+    return _rs_;
+}
+/* ********************************************************************************************* */
+ROBOT_KINEMATICS_T* _rk_;
+robot::robot_kinematics_t* PREPARE_ROBOT_KINEMATICS() {
+	if(_rk_ == 0) {
+		_rk_ = new ROBOT_KINEMATICS_T;
+		_rk_->init(PREPARE_ROBOT());
+	}
+	return _rk_;
+}
+/* ********************************************************************************************* */
+double _TOLERANCE_ = 1e-9;
+void ASSERT_MATRIX_EQ(Eigen::MatrixXd A, Eigen::MatrixXd B) 
+{
+    ASSERT_EQ(A.rows(), B.rows());
+    ASSERT_EQ(A.cols(), B.cols());
+    for(int i=0; i < A.rows(); ++i) {
+        for(int j=0; j < A.cols(); ++j) {
+            ASSERT_NEAR(A(i,j), B(i,j), _TOLERANCE_);
+        }
+    }
+}
+/* ********************************************************************************************* */
