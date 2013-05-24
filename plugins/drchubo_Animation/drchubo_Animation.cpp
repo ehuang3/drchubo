a91d7edabbbfbab06df8b82bf8a30ef2268d4ed4
ana-GT <ahuaman3@gatech.edu>
2013-05-24 18:23:50 -0400

    Added animation of pose to the existing joint animation, still needs work to look decent

--- plugins/drchubo_Animation/drchubo_Animation.cpp
+++ plugins/drchubo_Animation/drchubo_Animation.cpp
@@ -2,6 +2,7 @@
 #include "gazebo.hh"
 #include "common/common.hh"
 #include "physics/physics.hh"
+#include "../../src/utils/data_paths.h"
 
 std::string gJointNames[26] = {"drchubo::LSP", "drchubo::LSR", "drchubo::LSY", "drchubo::LEP", "drchubo::LWY", "drchubo::LWP", "drchubo::LWR",
 			       "drchubo::RSP", "drchubo::RSR", "drchubo::RSY", "drchubo::REP", "drchubo::RWY", "drchubo::RWP", "drchubo::RWR",
@@ -33,7 +34,7 @@ namespace gazebo
       float ang[gNumJoints]; int ind;
       int numTrajPoints = 900;
       FILE* pFile;
-      pFile = fopen("/home/ana/Research/drchubo/data/trajs/traj.txt", "r");
+      pFile = fopen(VRC_DATA_PATH "trajs/traj.txt", "r");
 
       if( pFile == NULL ) {
 	printf("Did not find file, exiting! \n");
@@ -58,16 +59,75 @@ namespace gazebo
 	for( int j = 0; j < gNumJoints; ++j ) {
 	  common::NumericKeyFrame * key = anim[ gJointNames[j] ]->CreateKeyFrame(t);
 	  key->SetValue( ang[j] );	  
-	  printf("val[%d]=%f\n", j, ang[j]);
 	}
 	// Advance one time step
 	t+=dt;
 
       }
+
+//////////////////////////////
+// Save stance
+    std::vector<int> mTrajStance;
+    mTrajStance.resize(0);
+
+    // Read file with trajectories and stance
+    FILE* sFile;
+    sFile = fopen(VRC_DATA_PATH "trajs/stance.txt", "r");
+    
+    if( sFile == NULL ) {
+      printf("Did not find either traj or stance file, exiting! \n");
+      return;
+    }
+    
+    for( int i = 0; i < numTrajPoints; ++i ) {
+      // Read stance
+      int stance;
+      fscanf( sFile, "%d \n", &stance );
+      
+      mTrajStance.push_back( stance );
+    }
+    
+    fclose( sFile );
+//////////////////////////////////
 	
       // Attach the animation to the model
       _model->SetJointAnimation( anim );
       printf("End loading animation \n");
+
+      /////////////////////////////////
+      // WORLD ANIMATION  		
+      gazebo::common::PoseAnimationPtr anim2(
+          new gazebo::common::PoseAnimation("test", 1000.0, true));
+
+      gazebo::common::PoseKeyFrame *key2;
+
+      t = 0;
+      double xdist = 0;
+      for( int i = 0; i < numTrajPoints; ++i ) {
+
+          key2 = anim2->CreateKeyFrame(t);
+	// Single does not move
+          if( mTrajStance[i] == 2 || mTrajStance[i] == 3 ) {
+
+	  }
+          else {
+           xdist += dt;
+          }
+
+            key2->SetTranslation(math::Vector3( xdist, 0, 1.0));
+            key2->SetRotation(math::Quaternion(0, 0, 0.0));
+
+	// Advance one time step
+	t+=dt;
+
+      }
+
+      _model->SetAnimation(anim2);
+
+	////////////////////
+
+
+
 
     }
   };
