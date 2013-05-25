a3a8ca5327ec64916ae25125a4aea1d000f3592d
ana-GT <ahuaman3@gatech.edu>
2013-05-25 02:39:59 -0400

    Added push world and files used for the 2 videos

--- plugins/drchubo_Animation/drchubo_Animation.cpp
+++ plugins/drchubo_Animation/drchubo_Animation.cpp
@@ -1,3 +1,6 @@
+/**
+ * @file drchubo_Animation.cpp
+ */
 #include <map>
 #include "gazebo.hh"
 #include "common/common.hh"
@@ -17,23 +20,28 @@ namespace gazebo
    */
   class drchubo_Animation : public ModelPlugin
   {
-    public: void Load(physics::ModelPtr _model, sdf::ElementPtr /*_sdf*/)
+  public: void Load(physics::ModelPtr _model, sdf::ElementPtr /*_sdf*/)
     {
-      printf("Loading animation \n");
-      _model->SetGravityMode( false );
-      std::map<std::string, common::NumericAnimationPtr> anim;
-      double T = 10.0;
+      printf( "*** Loading animation ***\n" );
 
-      // Fill joint init
+      // Set gravity to false, otherwise the robot slowly falls
+      _model->SetGravityMode( false );
+      std::map<std::string, common::NumericAnimationPtr> joint_anim;
+      common::NumericKeyFrame *joint_key;
+
+      // Fill joint initialization
+      double T = 10.0;
       for( int i = 0; i < gNumJoints; ++i ) {
-	anim[ gJointNames[i] ].reset( new common::NumericAnimation( "anim", T, true) );
+	joint_anim[ gJointNames[i] ].reset( new common::NumericAnimation( "anim", T, true) );
       }
 
-
+      //**********************************
       // Read file with trajectories
+      //**********************************
       float ang[gNumJoints]; int ind;
       int numTrajPoints = 900;
       FILE* pFile;
+
       pFile = fopen(VRC_DATA_PATH "trajs/traj.txt", "r");
 
       if( pFile == NULL ) {
@@ -42,94 +50,54 @@ namespace gazebo
       }
 
       double t;
-
       double dt = T / (double)numTrajPoints;
 
       t = 0;
       for( int i = 0; i < numTrajPoints; ++i ) {
-
+	
 	fscanf( pFile, "%d %f %f %f %f %f %f", &ind, &ang[0], &ang[1], &ang[2], &ang[3], &ang[4], &ang[5] ); 
 	ang[6] = 0.0;
 	fscanf( pFile, "%f %f %f %f %f %f", &ang[7], &ang[8], &ang[9], &ang[10], &ang[11], &ang[12] );
 	ang[13] = 0.0;
 	fscanf( pFile, "%f %f %f %f %f %f", &ang[14], &ang[15], &ang[16], &ang[17], &ang[18], &ang[19] );
 	fscanf( pFile, "%f %f %f %f %f %f \n", &ang[20], &ang[21], &ang[22], &ang[23], &ang[24], &ang[25] );
-
+	
 	// Create a key frame for each trajectory point read
 	for( int j = 0; j < gNumJoints; ++j ) {
-	  common::NumericKeyFrame * key = anim[ gJointNames[j] ]->CreateKeyFrame(t);
-	  key->SetValue( ang[j] );	  
+	  joint_key = joint_anim[ gJointNames[j] ]->CreateKeyFrame(t);
+	  joint_key->SetValue( ang[j] );	  
 	}
 	// Advance one time step
 	t+=dt;
-
       }
 
-//////////////////////////////
-// Save stance
-    std::vector<int> mTrajStance;
-    mTrajStance.resize(0);
-
-    // Read file with trajectories and stance
-    FILE* sFile;
-    sFile = fopen(VRC_DATA_PATH "trajs/stance.txt", "r");
-    
-    if( sFile == NULL ) {
-      printf("Did not find either traj or stance file, exiting! \n");
-      return;
-    }
-    
-    for( int i = 0; i < numTrajPoints; ++i ) {
-      // Read stance
-      int stance;
-      fscanf( sFile, "%d \n", &stance );
-      
-      mTrajStance.push_back( stance );
-    }
-    
-    fclose( sFile );
-//////////////////////////////////
-	
       // Attach the animation to the model
-      _model->SetJointAnimation( anim );
-      printf("End loading animation \n");
-
-      /////////////////////////////////
-      // WORLD ANIMATION  		
-      gazebo::common::PoseAnimationPtr anim2(
-          new gazebo::common::PoseAnimation("test", T, true));
-
-      gazebo::common::PoseKeyFrame *key2;
+      _model->SetJointAnimation( joint_anim );
+      printf("End loading Joint animation \n");
+      
+      //*********************
+      // POSE ANIMATION
+      //*********************
+      gazebo::common::PoseAnimationPtr pose_anim( new gazebo::common::PoseAnimation("test", T, true) );
+      gazebo::common::PoseKeyFrame *pose_key;
 
       t = 0;
-      double xdist = 0;
+
       for( int i = 0; i < numTrajPoints; ++i ) {
 
-          key2 = anim2->CreateKeyFrame(t);
-	// Single does not move
-          if( mTrajStance[i] == 2 || mTrajStance[i] == 3 ) {
+	pose_key = pose_anim->CreateKeyFrame(t);
 
-	  }
-          else {
-           xdist += dt;
-          }
-
-            key2->SetTranslation(math::Vector3( 0*xdist, 0, 1.0));
-            key2->SetRotation(math::Quaternion(0, 0, 0.0));
+	pose_key->SetTranslation(math::Vector3( t, 0, 1.0));
+	pose_key->SetRotation(math::Quaternion(0, 0, 0.0));
 
 	// Advance one time step
 	t+=dt;
-
       }
-
-      _model->SetAnimation(anim2);
-
-	////////////////////
-
-
-
-
+      
+      _model->SetAnimation( pose_anim );      
+      printf( "** End loading Pose animation **\n" );
     }
+
   };
 
   // Register this plugin with the simulator
--- plugins/drchubo_AnimationPush/drchubo_AnimationPush.cpp
+++ plugins/drchubo_AnimationPush/drchubo_AnimationPush.cpp
@@ -8,83 +8,99 @@
 #include "../../src/utils/data_paths.h"
 
 
-std::string gJointNames[26] = {"drchubo::LSP", "drchubo::LSR", "drchubo::LSY", "drchubo::LEP", "drchubo::LWY", "drchubo::LWP", "drchubo::LWR",
+std::string gJointNames[35] = {"drchubo::LSP", "drchubo::LSR", "drchubo::LSY", "drchubo::LEP", "drchubo::LWY", "drchubo::LWP", "drchubo::LWR",
 			       "drchubo::RSP", "drchubo::RSR", "drchubo::RSY", "drchubo::REP", "drchubo::RWY", "drchubo::RWP", "drchubo::RWR",
 			       "drchubo::LHY", "drchubo::LHR", "drchubo::LHP", "drchubo::LKP", "drchubo::LAP", "drchubo::LAR",
-			       "drchubo::RHY", "drchubo::RHR", "drchubo::RHP", "drchubo::RKP", "drchubo::RAP", "drchubo::RAR"};
-int gNumJoints = 26;
+			       "drchubo::RHY", "drchubo::RHR", "drchubo::RHP", "drchubo::RKP", "drchubo::RAP", "drchubo::RAR",
+			       "drchubo::TSY", "drchubo::NKY", "drchubo::NKP",
+			       "drchubo::LF1", "drchubo::LF2", "drchubo::LF3",
+			       "drchubo::RF1", "drchubo::RF2", "drchubo::RF3"};
+int gNumJoints = 35;
 
 namespace gazebo
 {
   /**
-   * @class drchubo_Animation
+   * @class drchubo_AnimationPush
    */
-  class drchubo_Animation : public ModelPlugin
+  class drchubo_AnimationPush : public ModelPlugin
   {
     public: void Load(physics::ModelPtr _model, sdf::ElementPtr /*_sdf*/)
     {
-      printf("Loading animation \n");
+      printf( "*** Loading animation *** \n" );
+
+      // Set gravity to false, otherwise the robot slowly falls
       _model->SetGravityMode( false );
-      std::map<std::string, common::NumericAnimationPtr> anim;
-      double T = 10.0;
-      int numPoints = 500;
-      double dt = T / (double) numPoints;
-      // For testing we only will use shoulder pitch and yaw
-      anim["drchubo::LSR"].reset( new common::NumericAnimation( "anim", T, true) );
-      //anim["drchubo::LSY"].reset( new common::NumericAnimation( "anim", T, true) );
+
+      std::map<std::string, common::NumericAnimationPtr> joint_anim;
+      common::NumericKeyFrame * joint_key; 
+
+
+      // Fill joint initialization
+      double T = 3.0;
+      int numTrajPoints = 500;
+
+      for( int i = 0; i < gNumJoints; ++i ) {
+	joint_anim[ gJointNames[i] ].reset( new common::NumericAnimation( "anim", T, true ) );	
+      }
+
 
       // Start storing
-      common::NumericKeyFrame * key; 
       double t = 0;
-//      for( int i = 0; i < numPoints; ++i ) {
+      double dt = T / (double) numTrajPoints;
+      for( int i = 0; i < numTrajPoints; ++i ) {
 
-	// Start with arm raised
-	key = anim[ "drchubo::LSR" ]->CreateKeyFrame(0);
-	key->SetValue( 0.2 );	  
+	for( int j = 0; j < gNumJoints; ++j ) {
+	  joint_key = joint_anim[ gJointNames[j] ]->CreateKeyFrame(t);
+	  if( j == 0 ) { // LSP (raise arm)
+	    joint_key->SetValue(-1.0 ); 
+	  }
+	  else if( j == 1 ) { // LSR (away from body)
+	    joint_key->SetValue( 0.35 );
+	  }
+	  else if( j == 2 ) { // LSY (turn forearm towards inside)
+	    joint_key->SetValue( -1.5 );
+	  }
+	  else if( j == 3 ) { // LEP
+	    joint_key->SetValue( -1.57*( (double)i/(double)numTrajPoints ) );
+	  }
+	  else {
+	    joint_key->SetValue(0);
+	  }
+	}
 
-	key = anim[ "drchubo::LSR" ]->CreateKeyFrame(T);
-//	double x = ((double)i/(double)numPoints)*0.6;
-	key->SetValue( 0.6 );	  
 	t += dt;
-//      }
-
-
+      }
 
 	
       // Attach the animation to the model
-      _model->SetJointAnimation( anim );
-      printf("End loading animation \n");
+      _model->SetJointAnimation( joint_anim );
+      printf("*** End loading animation *** \n");
 
-      /////////////////////////////////
-      // WORLD ANIMATION  		
-      gazebo::common::PoseAnimationPtr anim2(new gazebo::common::PoseAnimation("test", T, true));
+      //************************
+      // WORLD ANIMATION  	
+      //************************
+      gazebo::common::PoseAnimationPtr pose_anim( new gazebo::common::PoseAnimation("test", T, true) );
+      gazebo::common::PoseKeyFrame *pose_key;
 
-      gazebo::common::PoseKeyFrame *key2;
+      t = 0;
 
-      key2 = anim2->CreateKeyFrame(0);
-      key2->SetTranslation(math::Vector3( 0, 0, 1.25));
-      key2->SetRotation(math::Quaternion(0, 0, 0.0));
+      for( int i = 0; i < numTrajPoints; ++i ) {
 
-      key2 = anim2->CreateKeyFrame(5.0);
-      key2->SetTranslation(math::Vector3( 0.0, 0.0, 1.25));
-      key2->SetRotation(math::Quaternion(0, 0, 0, 0.0));
+	pose_key = pose_anim->CreateKeyFrame(t);
 
-      key2 = anim2->CreateKeyFrame(10.0);
-      key2->SetTranslation(math::Vector3( 0.0, 0.0, 1.25));
-      key2->SetRotation(math::Quaternion(0, 0, 0, 0.0));
+	pose_key->SetTranslation( math::Vector3( 0.0, 0, 0.95) );
+	pose_key->SetRotation(math::Quaternion(0, 0, 0.0));
 
-
-
-      _model->SetAnimation(anim2);
-
-	////////////////////
-
-
-
-
+	// Advance one time step
+	t+=dt;
+      }
+      
+      _model->SetAnimation( pose_anim );      
+      printf( "** End loading Pose animation **\n" );
+    
     }
   };
 
   // Register this plugin with the simulator
-  GZ_REGISTER_MODEL_PLUGIN(drchubo_Animation)
+  GZ_REGISTER_MODEL_PLUGIN(drchubo_AnimationPush)
 }
